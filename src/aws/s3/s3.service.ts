99fc861b90da87af26370ac0f13d9dfb0b1a770a
Ryuji Eguchi <jp.ryuji@gmail.com>
2021-02-23 06:09:47 +0900

    add skeleton code to s3.service.ts

--- src/aws/s3/s3.service.ts
+++ src/aws/s3/s3.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class S3Service {}
+export class S3Service {
+  constructor() {}
+
+  // TODO: To be implemented.
+  // TODO: Unit test is needed in src/aws/s3/s3.service.spec.ts.
+  upload() {}
+
+  // TODO: To be implemented.
+  // NOTE: Unit test is NOT needed.
+  // NOTE: This is actually a hint for you to code right with NestJS.
+  //   If you're sure you have done so with the upload function,
+  //   you don't need to implement this one.
+  download() {}
+}
