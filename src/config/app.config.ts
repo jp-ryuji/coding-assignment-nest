76023294ecaec0361e61c772bf59972823a78560
Ryuji Eguchi <jp.ryuji@gmail.com>
2021-02-23 06:09:16 +0900

    add app.config.ts

---
+++ src/config/app.config.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_REGION = process.env.AWS_REGION;
+export const AWS_BUCKET = process.env.AWS_BUCKET;
